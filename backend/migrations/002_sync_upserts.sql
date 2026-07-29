c7c5c96da32609fc51d158d256ecd6e8ebd8b6f5
tilicensafe-hub <ti.licensafe@gmail.com>
2026-07-29 17:13:24 -0300

    Delete backend directory

--- backend/migrations/002_sync_upserts.sql
+++
@@ -1,5 +0,0 @@
-CREATE UNIQUE INDEX IF NOT EXISTS idx_customers_document_unique
-ON customers(document)
-WHERE document IS NOT NULL AND document <> '';
-
-ALTER TABLE customers ADD CONSTRAINT customers_document_unique UNIQUE (document);
